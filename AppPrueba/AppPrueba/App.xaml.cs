94da49f37aabda78bac8eebe7b6ce255d6157422
Irissf <iris.seijo@gmail.com>
2021-12-29 11:56:40 +0100

    Agregar archivos de proyecto.

---
+++ AppPrueba/AppPrueba/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPrueba
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //añadimos la pantalla que queremos que se inicie
+            //debemos meterle el Navigation, para indicar la página raiz, y que funcione la navegación entre
+            //las páginas
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+
+        //CICLOS DE VIDA 
+
+        protected override void OnStart()
+        {
+            //Cuando inicia la app
+        }
+
+        protected override void OnSleep()
+        {
+            //cuando está en segundo plano
+        }
+
+        protected override void OnResume()
+        {
+            //cuando la app pasa de segundo a plano a despertar
+        }
+    }
+}
---
+++ AppPrueba/AppPrueba/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPrueba
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        //partial indica que la totalidad de la clase no está totalmente definida aqui
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Inicializar(object sender, EventArgs e)
+        {
+            labelTexto.Text = entryTexto.Text;
+        }
+
+        private void CambiarPes(object sender, EventArgs e)
+        {
+            //movernos a otra página
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
